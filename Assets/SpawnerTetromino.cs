ff633a8b30dd7043506bdec706f2419cf2e42054
Szymon <68813815+SzymonWindak@users.noreply.github.com>
2020-07-26 16:15:36 +0200

    Add Assets from Game

---
+++ Assets/SpawnerTetromino.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerTetromino : MonoBehaviour {
+
+    public GameObject[] Tetrominos;
+
+	// Use this for initialization
+	void Start () {
+        NewTetromino();
+
+    }
+	
+	// Update is called once per frame
+	public void NewTetromino () {
+        Instantiate(Tetrominos[Random.Range(0, Tetrominos.Length)], transform.position, Quaternion.identity);
+	}
+}
